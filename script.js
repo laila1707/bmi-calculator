f4e5cfbe04489f2cdde451689cb784ba74abfa33
lailanurfardah <lailanurfardah1707@gmail.com>
2023-04-12 21:05:12 +0700

    add getid js

---
+++ script.js
@@ -0,0 +1,6 @@
+let age = document.getElementById("age");
+let height = document.getElementById("height");
+let weight = document.getElementById("weight");
+let male = document.getElementById("m");
+let female = document.getElementById("f");
+let form = document.getElementById("form");
